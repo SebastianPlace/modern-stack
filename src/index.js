321d0bcc8cf8a3e4823e5d93bb27b1e40f61c10b
Sebastian Place <sebplace94@gmail.com>
2017-07-21 13:06:42 +0100

    Add eslint with aribnb plugins and compat.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 import Dog from './dog'
 
-const olly = new Dog('Olly')
+const apiRoot = 'https://jsonplaceholder.typicode.com'
 
+const olly = new Dog('Olly')
 console.log(olly.bark())
