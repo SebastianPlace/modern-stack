bfe954d3bb0c13202ee934e980f04d9d16aad9f7
Sebastian Place <sebplace94@gmail.com>
2017-07-21 13:19:30 +0100

    Add flow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+// @flow
+
 /* eslint-disable no-console */
 import Dog from './dog'
-
-const apiRoot = 'https://jsonplaceholder.typicode.com'
 
 const olly = new Dog('Olly')
 console.log(olly.bark())
