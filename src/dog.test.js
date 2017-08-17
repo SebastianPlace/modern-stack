cda25f5939573277beb9dfc04ba603a070a7393c
Sebastian Place <sebplace94@gmail.com>
2017-08-17 20:58:13 +0100

    Ignore some node_modules flow

--- src/dog.test.js
+++
@@ -1,6 +0,0 @@
-import Dog from './dog'
-
-it('Dog.bark', () => {
-  const testDog = new Dog('Test')
-  expect(testDog.bark()).toBe('Wah wah, I am Test')
-})
--- src/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-/* eslint-disable no-console */
-import Dog from './dog'
-
-const olly = new Dog('Olly')
-console.log(olly.bark())
