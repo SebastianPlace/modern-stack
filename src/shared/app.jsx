0b2becb0f3dac802d6d8f8ffbece54fcf64b2230
Sebastian Place <sebplace94@gmail.com>
2017-08-22 22:39:44 +0100

    Add server side rendering

--- src/shared/app.jsx
+++ src/shared/app.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { Switch } from 'react-router'
 import { Route } from 'react-router-dom'
 
-import { APP_NAME } from '../shared/config'
+import { APP_NAME } from './config'
 import Nav from './component/nav'
 import HomePage from './component/page/home'
 import HelloPage from './component/page/hello'
@@ -14,7 +14,7 @@ import {
   HOME_PAGE_ROUTE,
   HELLO_PAGE_ROUTE,
   HELLO_ASYNC_PAGE_ROUTE,
-} from '../shared/routes'
+} from './routes'
 
 const App = () => (
   <div>
