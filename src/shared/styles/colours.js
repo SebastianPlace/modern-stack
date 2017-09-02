0c5902e0b0798f8abb9f26622fcfc981e83596f7
Sebastian Place <sebplace94@gmail.com>
2017-09-02 15:35:01 +0100

    Add return type to darken

--- src/shared/styles/colours.js
+++ src/shared/styles/colours.js
@@ -5,6 +5,6 @@ export const darkText = '#2b2b2b'
 export const primary = '#685eb2'
 
 // Modifier Functions
-export function darken(colour: string, level: number = 0.2) {
+export function darken(colour: string, level: number = 0.2): string {
   return Color(colour).darken(level).string()
 }
